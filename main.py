561ef883e779898bbd1e024f9f525898b1019577
LGgameLAB <45958481+LGgameLAB@users.noreply.github.com>
2023-01-19 23:09:52 +0000

    basic linear polygon analyzing complete

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 from fontTools.ttLib import TTFont
-font = TTFont('font.ttf')
-print(font.__dict__)
+from pygame import Vector2 as Vec
+font1 = TTFont('PerfectDOSVGA437.ttf')
+# for key, value in font.reader.tables.items():
+#     print(key, value)
+# print(font1["glyf"]["f"].__dict__)
+# print(vars(font.reader.tables["glyf"]))
+
+def transform(l, scale, shift):
+    l2 = []
+    for v in l:
+        new = Vec(v)*scale + shift
+        l2.append((new.x, new. y))
+
+    return l2
+
+def getPolysFromString(font, phrase):
+    polys = []
+    x2 = 0
+    for c in [*phrase]:
+        charData = font["glyf"][c]
+        for x in range(charData.numberOfContours):
+            start = 0 if x == 0 else charData.endPtsOfContours[x-1]+1
+            end = charData.endPtsOfContours[x]
+            polys.append(transform(charData.coordinates[start:end+1], 1/256, (7*x2, 0)))
+        x2 += 1
+    
+    return polys
+
+for x in [f"polygon({str(v)})" for v in getPolysFromString(font1, "Ellis")]:
+    print(x + ", ", end="") 
